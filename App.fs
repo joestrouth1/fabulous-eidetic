dfcf06202d3448cf2d3291d26ed745e1888e052e
Joe Strouth <joestrouth1@gmail.com>
2025-04-09 19:10:09 -0400

    refactor: remove Option from Cell type, split update function

--- App.fs
+++ App.fs
@@ -14,13 +14,11 @@ module Grid =
         | Hidden
 
     type Cell =
-        Option<
-            {| value: int
-               label: string
-               state: CellState |}
-         >
+        { value: int
+          label: string
+          state: CellState }
 
-    type Grid = Cell list list
+    type Grid = Cell option list list
     let Dimensions = {| rows = 6; columns = 3 |}
     let ValueRange = [ 1..9 ]
 
@@ -30,12 +28,12 @@ module Grid =
 
         let totalCells = Dimensions.rows * Dimensions.columns
 
-        let filledCells: List<Cell> =
+        let filledCells: List<Cell option> =
             shuffledValues
             |> List.map (fun (value) ->
-                {| value = value
-                   label = sprintf "%d" value
-                   state = Visible |})
+                { value = value
+                  label = sprintf "%d" value
+                  state = Visible })
             |> List.map Some
 
         let emptyCells = List.init (totalCells - List.length shuffledValues) (fun _ -> None)
@@ -86,64 +84,73 @@ module App =
 
     let init () = initModel, Cmd.none
 
+
+    let transformGridToObscured (grid: Grid) =
+        grid
+        |> List.map (fun row ->
+            row
+            |> List.map (fun cell ->
+                Option.map
+                    (fun cell ->
+                        if cell.state = Visible then
+                            { cell with state = Obscured }
+                        else
+                            cell)
+                    cell))
+
+    let advanceState model =
+        match model.State with
+        | Start ->
+            { model with
+                State = Learning
+                grid = Grid.init () },
+            Cmd.none
+        | Learning ->
+            { model with
+                State = Testing
+                grid = transformGridToObscured model.grid },
+            Cmd.none
+        | Testing -> { model with State = End }, Cmd.none
+        | End -> { model with State = Start }, Cmd.none
+
+    let updateGridCell row col grid : Grid =
+        grid
+        |> List.mapi (fun rowIndex gridRow ->
+            if rowIndex = row then
+                gridRow
+                |> List.mapi (fun colIndex cellOption ->
+                    match cellOption with
+                    | Some c when colIndex = col -> Some { c with state = Hidden }
+                    | c -> c)
+            else
+                gridRow
+                |> List.map (fun cell ->
+                    Option.map
+                        (fun cell ->
+                            if cell.state = Hidden then
+                                cell
+                            else
+                                { cell with state = Obscured })
+                        cell))
+
+    let pressCell row col model =
+        match model.grid[row][col] with
+        | Some cell ->
+            let newGrid = updateGridCell row col model.grid
+
+            { model with
+                grid = newGrid
+                State = Testing
+                message = Some $"Pressed {cell.value}" },
+            Cmd.none
+        | None -> { model with message = None }, Cmd.none
+
     let update msg model =
         match msg with
         | SetState state -> { model with State = state }, Cmd.none
-        | AdvanceState ->
-            match model.State with
-            | Start ->
-                { model with
-                    State = Learning
-                    grid = Grid.init () },
-                Cmd.none
-            | Learning ->
-                { model with
-                    State = Testing
-                    grid =
-                        model.grid
-                        |> List.map (fun row ->
-                            row
-                            |> List.map (fun cell ->
-                                match cell with
-                                | Some cell when cell.state = Visible ->
-                                    Some
-                                        {| label = cell.label
-                                           state = Obscured
-                                           value = cell.value |}
-                                | None -> None
-                                | _ -> cell)) },
-                Cmd.none
-            | Testing -> { model with State = End }, Cmd.none
-            | End -> { model with State = Start }, Cmd.none
-        | PressCell(row, col) ->
-            let result =
-                match model.grid[row][col] with
-                | Some cell ->
-                    let newGrid =
-                        model.grid
-                        |> List.mapi (fun rowIndex gridRow ->
-                            if rowIndex <> row then
-                                gridRow
-                                |> List.map (fun cell ->
-                                    match cell with
-                                    | Some cell when cell.state = Hidden -> Some cell
-                                    | Some cell -> Some {| cell with state = Obscured |}
-                                    | None -> None)
-                            else
-                                gridRow
-                                |> List.mapi (fun colIndex cellOption ->
-                                    match cellOption with
-                                    | Some c when colIndex = col -> Some {| c with state = Hidden |}
-                                    | Some c when c.state = Visible -> Some {| c with state = Obscured |}
-                                    | c -> c))
+        | AdvanceState -> advanceState model
+        | PressCell(row, col) -> pressCell row col model
 
-                    { model with
-                        grid = newGrid
-                        State = Testing
-                        message = Some $"Pressed {cell.value}" }
-                | None -> { model with message = None }
-
-            result, Cmd.none
 
     let view model =
         SplitView(
